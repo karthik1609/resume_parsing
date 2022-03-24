0d0a95baa1f15689e9aea5f7879857d905685579
Karthik <karthik@aimlytics.ai>
2022-03-24 17:19:04 +0530

    cleanup

--- include/ResumeParser.py
+++
@@ -1,7 +0,0 @@
-def extract_phone_number(self):
-    items = re.finditer(r'\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}', resume)
-    return [(resume[item.span()[0]:item.span()[1]], item.span()) for item in items]
-    
-    
-    
-        
